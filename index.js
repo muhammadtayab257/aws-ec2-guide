1c03ed86de6ba2d34b3623478847ea0e10901776
Tayyab Siraj <60255376+muhammadtayab257@users.noreply.github.com>
2025-07-12 03:23:13 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const express = require("express");
 const app = express();
-const axios = require("axios");
-const os = require("os");
-const { execSync } = require("child_process");
-const used = process.memoryUsage();
 require("dotenv").config();
+
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
@@ -14,4 +11,3 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
-
