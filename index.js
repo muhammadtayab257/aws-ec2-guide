d69a197906ca1712495d38c627729cd57bd0e652
Tayyab Siraj <60255376+muhammadtayab257@users.noreply.github.com>
2026-01-13 21:10:25 +0500

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.get("/", (req, res) => {
   res.json({ message: "Hello, World i have been deployed to aws ec2 server" });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+// Listen on all network interfaces
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
