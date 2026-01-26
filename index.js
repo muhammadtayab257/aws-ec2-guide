fd49b47157572b96702e63764b8fd846e7ac1377
Tayyab siraj <tayyab.siraj@idnetifymobile.com>
2026-01-27 03:27:01 +0500

    added the ci/cd pipelines

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.get("/", (req, res) => {
 });
 
 // Listen on all network interfaces
-app.listen(PORT, "localhost", () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
+
