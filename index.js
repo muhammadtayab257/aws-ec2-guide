092abb6db1c9366e70ab0ca9ded426f59282b462
Tayyab siraj <tayyab.siraj@idnetifymobile.com>
2026-01-27 01:57:58 +0500

    minor

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000; 
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello, World i have been deployed to aws ec2 server" });
